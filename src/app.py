f19e98e2095ff104211f5a0f5e6cbd91de36be85
Ishwar Gupta <76861400+ish-g@users.noreply.github.com>
2023-11-11 13:29:34 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -80,7 +80,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div([
-    html.H2(children="Covid-19 Statistics Across Globe", style={'color': '#fff', 'text-align': 'center'}),
+    html.H2(children="Covid-19 Statistics Across Globe", style={'color': 'black', 'text-align': 'center'}),
     html.Div([
         html.Div([
             html.Div([
